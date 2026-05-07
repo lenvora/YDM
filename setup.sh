e661d69221b671ac41ec5d1f16a86bdfff125732
lenvora <nelvora0@gmail.com>
2026-05-08 00:48:17 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ echo "[Desktop Entry]
 Name=YDM
 Comment=YouTube Download Manager
 Exec=$APP_PATH/venv/bin/python $APP_PATH/main.py
-Icon=$APP_PATH/icon_1.png
+Icon=$APP_PATH/icon_2.png
 Terminal=false
 Type=Application
 Categories=Network;Utility;" > $DESKTOP_FILE
